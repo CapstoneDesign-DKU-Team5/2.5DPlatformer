f0615fc6b96d797b5f4da6b4f3d1c5b4e0a1e6a7
yoonseungeon <yoonseungeon@naver.com>
2025-05-20 13:08:31 +0900

    Monster Attack 1

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,10 +1,11 @@
-    using System.Collections;
-    using UnityEngine;
-    using UnityEngine.AI;
-    using UnityEngine.SocialPlatforms;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SocialPlatforms;
 
-    public class Monster : MonoBehaviour
-    {
+public class Monster : MonoBehaviour
+{
     [Header("컴포넌트 참조")]
     [SerializeField, Tooltip("자식 BoxCollider (충돌 판정용)")]
     protected BoxCollider childBoxCollider;
@@ -52,175 +53,190 @@
 
 
     protected virtual void Awake()
+    {
+        monsterXOrZ = Approximately(Mathf.Abs(transform.eulerAngles.y % 180f));
+
+        childBoxCollider = transform.Find("ChaseRange").GetComponentInChildren<BoxCollider>();
+
+        rigidBody = GetComponent<Rigidbody>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        navMeshAgent.enabled = false;
+        navMeshAgent.updateRotation = false;
+
+        state = State.IDLE;
+        HP = 3;
+        StartCoroutine(StateMachine());
+    }
+
+    protected IEnumerator StateMachine()
+    {
+        while (HP > 0)
         {
-            monsterXOrZ = Approximately(Mathf.Abs(transform.eulerAngles.y % 180f));
-
-            childBoxCollider = transform.Find("ChaseRange").GetComponentInChildren<BoxCollider>();
-
-            rigidBody = GetComponent<Rigidbody>();
-            spriteRenderer = GetComponent<SpriteRenderer>();
-
-            animator = GetComponent<Animator>();
-            navMeshAgent = GetComponent<NavMeshAgent>();
-
-            navMeshAgent.enabled = false;
-            navMeshAgent.updateRotation = false;
-
-            state = State.IDLE;
-            HP = 3;
-            StartCoroutine(StateMachine());
+            yield return StartCoroutine(state.ToString());
         }
-
-        protected IEnumerator StateMachine()
+    }
+    protected IEnumerator IDLE()
+    {
+        AnimatorStateInfo curAnimStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (curAnimStateInfo.IsName("IdleNormal") == false)
         {
-            while (HP > 0)
-            {
-                yield return StartCoroutine(state.ToString());
-            }
+            animator.Play("IdleNormal", 0, 0);
         }
-        protected IEnumerator IDLE()
+        yield return null;
+    }
+
+    protected IEnumerator CHASE()
+    {
+        AnimatorStateInfo curAnimStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (curAnimStateInfo.IsName("Walk") == false)
         {
-            AnimatorStateInfo curAnimStateInfo = animator.GetCurrentAnimatorStateInfo(0);
-            if (curAnimStateInfo.IsName("IdleNormal") == false)
-            {
-                animator.Play("IdleNormal", 0, 0);
-            }
+            animator.Play("Walk", 0, 0);
+
             yield return null;
         }
 
-        protected IEnumerator CHASE()
+        //Debug.Log("범위 내부로 들어옴");
+
+        float vDistance = Mathf.Abs(transform.position.y - target.position.y);
+
+        float remainingDistance = monsterXOrZ ? target.position.x - transform.position.x : target.position.z - transform.position.z;
+
+        spriteRenderer.flipX = remainingDistance < 0;
+
+        remainingDistance = Mathf.Abs(remainingDistance);
+
+        if (remainingDistance <= navMeshAgent.stoppingDistance && vDistance < 1f && isSameDir(target))
         {
-            AnimatorStateInfo curAnimStateInfo = animator.GetCurrentAnimatorStateInfo(0);
-            if (curAnimStateInfo.IsName("Walk") == false)
-            {
-                animator.Play("Walk", 0, 0);
+            Debug.Log("Attack");
+            state = State.ATTACK;
 
-                yield return null;
-            }
-
-            //Debug.Log("범위 내부로 들어옴");
-
-            float vDistance = Mathf.Abs(transform.position.y - target.position.y);
-
-            float remainingDistance = monsterXOrZ ? target.position.x - transform.position.x : target.position.z - transform.position.z;
-
-            spriteRenderer.flipX = remainingDistance < 0;
-
-            remainingDistance = Mathf.Abs(remainingDistance);
-
-            if (remainingDistance <= navMeshAgent.stoppingDistance && vDistance < 1f && isSameDir(target)) 
-            {
-                Debug.Log("Attack");
-                //state = State.ATTACK;
-
-            }
-            else if (remainingDistance > (childBoxCollider.size.x / 2) || !isSameDir(target) || vDistance > 5f)
-            {
-                target = null;
-                navMeshAgent.SetDestination(transform.position);
-                yield return null;
-                state = State.IDLE;
-            }
-            else
-            {
-                yield return new WaitForSeconds(curAnimStateInfo.length);
-            }
         }
-
-        public void TriggerSetTarget(Transform t)
+        else if (remainingDistance > (childBoxCollider.size.x / 2) || !isSameDir(target) || vDistance > 5f)
         {
-            if (isSameDir(t) && state != State.CHASE)
-            {
-                target = t;
-                navMeshAgent.enabled = true;
-
-                CalTargetPos();
-
-                navMeshAgent.SetDestination(targetPos);
-
-                state = State.CHASE;
-            }
+            target = null;
+            navMeshAgent.SetDestination(transform.position);
+            yield return null;
+            state = State.IDLE;
         }
-
-        protected bool isSameDir(Transform t)
+        else
         {
-            bool playerXOrZ = Approximately(Mathf.Abs(t.eulerAngles.y) % 180);
-            //Debug.Log(playerXOrZ == monsterXOrZ);
-            return playerXOrZ == monsterXOrZ;
-        }
-
-        protected void CalTargetPos()
-        {
-            targetPos = target.position;
-            targetPos.y = transform.position.y;
-
-            if (monsterXOrZ)
-            {
-                targetPos.z = transform.position.z;
-            }
-            else
-            {
-                targetPos.x = transform.position.x;
-            }
-        }
-
-        protected bool Approximately(float num, float epsilon = 0.01f)
-        {
-            return Mathf.Abs(num) < epsilon;
-        }
-
-        protected IEnumerator ATTACK()
-        {
-            Debug.Log("공격중");
-            yield return new WaitForSeconds(2f);
-            state = State.CHASE;
-        }
-
-        protected IEnumerator KILLED()
-        {
-            return null;
-        }
-
-        protected virtual void Update()
-        {
-            if (target == null)
-            {
-                return;
-            }
-            CalTargetPos();
-            navMeshAgent.SetDestination(targetPos);
-        }
-
-        public virtual void OnDamaged(Vector3 attackerPos)
-        {
-
-            if (damaged)
-            {
-                return;
-            }
-
-            int dir;
-            rigidBody.linearVelocity = Vector3.zero;
-
-            if (monsterXOrZ)
-            {
-                dir = transform.position.x - attackerPos.x > 0 ? 1 : -1;
-                Vector3 dirVec = new Vector3(dir * 2, 0, 0);
-                rigidBody.AddForce(dirVec, ForceMode.Impulse);
-            }
-            else
-            {
-                dir = transform.position.z - attackerPos.z > 0 ? 1 : -1;
-                Vector3 dirVec = new Vector3(0, 0, dir * 2);
-                rigidBody.AddForce(dirVec, ForceMode.Impulse);
-            }
-
-            damaged = true;
-            Invoke("OffDamaged", 0.6f);
-        }
-
-        protected virtual void OffDamaged()
-        {
-            damaged = false;
+            yield return new WaitForSeconds(curAnimStateInfo.length);
         }
     }
+
+    public void TriggerSetTarget(Transform t)
+    {
+        if (isSameDir(t) && state != State.CHASE/* && state != State.ATTACK*/)
+        {
+            Debug.Log("trigger");
+            target = t;
+            navMeshAgent.enabled = true;
+
+            CalTargetPos();
+
+            navMeshAgent.SetDestination(targetPos);
+
+            state = State.CHASE;
+        }
+    }
+
+    protected bool isSameDir(Transform t)
+    {
+        bool playerXOrZ = Approximately(Mathf.Abs(t.eulerAngles.y) % 180);
+        //Debug.Log(playerXOrZ == monsterXOrZ);
+        return playerXOrZ == monsterXOrZ;
+    }
+
+    protected void CalTargetPos()
+    {
+        targetPos = target.position;
+        targetPos.y = transform.position.y;
+
+        if (monsterXOrZ)
+        {
+            targetPos.z = transform.position.z;
+        }
+        else
+        {
+            targetPos.x = transform.position.x;
+        }
+    }
+
+    protected bool Approximately(float num, float epsilon = 0.01f)
+    {
+        return Mathf.Abs(num) < epsilon;
+    }
+
+    protected IEnumerator ATTACK()
+    {
+        AnimatorStateInfo curAnimStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        animator.Play("Attack", 0, 0);
+
+        float vDistance = Mathf.Abs(transform.position.y - target.position.y);
+        float remainingDistance = monsterXOrZ ? target.position.x - transform.position.x : target.position.z - transform.position.z;
+        remainingDistance = Mathf.Abs(remainingDistance);
+
+        if (remainingDistance > navMeshAgent.stoppingDistance || vDistance > 1f || !isSameDir(target))
+        {
+            state = State.CHASE;
+        }
+        else
+        {
+            yield return new WaitForSeconds(curAnimStateInfo.length * 2f);
+        }
+
+    }
+
+    protected IEnumerator KILLED()
+    {
+        return null;
+    }
+
+    protected virtual void Update()
+    {
+        if (target == null || state == State.ATTACK)
+        {
+            return;
+        }
+        CalTargetPos();
+        navMeshAgent.SetDestination(targetPos);
+    }
+
+    public virtual void OnDamaged(Vector3 attackerPos)
+    {
+
+        if (damaged)
+        {
+            return;
+        }
+
+        int dir;
+        rigidBody.linearVelocity = Vector3.zero;
+
+        if (monsterXOrZ)
+        {
+            dir = transform.position.x - attackerPos.x > 0 ? 1 : -1;
+            Vector3 dirVec = new Vector3(dir * 2, 0, 0);
+            rigidBody.AddForce(dirVec, ForceMode.Impulse);
+        }
+        else
+        {
+            dir = transform.position.z - attackerPos.z > 0 ? 1 : -1;
+            Vector3 dirVec = new Vector3(0, 0, dir * 2);
+            rigidBody.AddForce(dirVec, ForceMode.Impulse);
+        }
+
+        damaged = true;
+        Invoke("OffDamaged", 0.6f);
+    }
+
+    protected virtual void OffDamaged()
+    {
+        damaged = false;
+    }
+}
